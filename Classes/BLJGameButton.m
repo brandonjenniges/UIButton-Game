0b9e7fcae996416c66d414dea9b2ef0c0e35e678
Brandon Jenniges <steveholt55@gmail.com>
2015-07-18 15:40:57 -0500

    Makes button color go back to original color after animation

--- Classes/BLJGameButton.m
+++ Classes/BLJGameButton.m
@@ -14,68 +14,66 @@
 @implementation BLJGameButton
 
 - (void)correct {
-	[self correct:nil];
+    [self correct:nil];
 }
 
 - (void)correct:(void (^)())onCompletion {
-	[self setTitleColor:[BLJGameButton correctColor] forState:UIControlStateNormal];
-
-	float animationDuration = 1.0;
-
-	CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"transform"];
-	anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-	anim.duration = animationDuration;
-	anim.repeatCount = 1;
-	anim.removedOnCompletion = YES;
-	anim.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2, 1.2, 1.0)];
-	[self.layer addAnimation:anim forKey:nil];
-
-	int64_t delayInSeconds = animationDuration;
-	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-	dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
-		if (onCompletion) {
-		    onCompletion();
-		}
-		[self setTitleColor:[BLJGameButton defaultColor] forState:UIControlStateNormal];
-	});
+    UIColor*originalColor = [self titleColorForState:UIControlStateNormal];
+    [self setTitleColor:[BLJGameButton correctColor] forState:UIControlStateNormal];
+    
+    float animationDuration = 1.0;
+    
+    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"transform"];
+    anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    anim.duration = animationDuration;
+    anim.repeatCount = 1;
+    anim.removedOnCompletion = YES;
+    anim.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2, 1.2, 1.0)];
+    [self.layer addAnimation:anim forKey:nil];
+    
+    int64_t delayInSeconds = animationDuration;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+        if (onCompletion) {
+            onCompletion();
+        }
+        [self setTitleColor:originalColor forState:UIControlStateNormal];
+    });
 }
 
 - (void)incorrect {
-	[self incorrect:nil];
+    [self incorrect:nil];
 }
 
 - (void)incorrect:(void (^)())onCompletion {
-	[self setTitleColor:[BLJGameButton incorrectColor] forState:UIControlStateNormal];
-
-	float animationDuration = 1.0;
-	CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-	anim.values = @[[NSValue valueWithCATransform3D:CATransform3DMakeTranslation(-10.0f, 0.0f, 0.0f)], [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(10.0f, 0.0f, 0.0f)]];
-	anim.autoreverses = YES;
-	anim.repeatCount = 2.0f;
-	anim.duration = animationDuration / 4.f;
-
-	[self.layer addAnimation:anim forKey:nil];
-
-	int64_t delayInSeconds = animationDuration;
-	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-	dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
-		if (onCompletion) {
-		    onCompletion();
-		}
-		[self setTitleColor:[BLJGameButton defaultColor] forState:UIControlStateNormal];
-	});
-}
-
-+ (UIColor *)defaultColor {
-	return [UIColor colorWithRed:33 / 255.0f green:33 / 255.0f blue:33 / 255.0f alpha:1.0f];
+    UIColor*originalColor = [self titleColorForState:UIControlStateNormal];
+    [self setTitleColor:[BLJGameButton incorrectColor] forState:UIControlStateNormal];
+    
+    float animationDuration = 1.0;
+    CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    anim.values = @[[NSValue valueWithCATransform3D:CATransform3DMakeTranslation(-10.0f, 0.0f, 0.0f)], [NSValue valueWithCATransform3D:CATransform3DMakeTranslation(10.0f, 0.0f, 0.0f)]];
+    anim.autoreverses = YES;
+    anim.repeatCount = 2.0f;
+    anim.duration = animationDuration / 4.f;
+    
+    [self.layer addAnimation:anim forKey:nil];
+    
+    int64_t delayInSeconds = animationDuration;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+        if (onCompletion) {
+            onCompletion();
+        }
+        [self setTitleColor:originalColor forState:UIControlStateNormal];
+    });
 }
 
 + (UIColor *)correctColor {
-	return [UIColor colorWithRed:0 / 255.0f green:200 / 255.0f blue:0 / 255.0f alpha:1.0f];
+    return [UIColor colorWithRed:0 / 255.0f green:200 / 255.0f blue:0 / 255.0f alpha:1.0f];
 }
 
 + (UIColor *)incorrectColor {
-	return [UIColor colorWithRed:200 / 255.0f green:0 / 255.0f blue:0 / 255.0f alpha:1.0f];
+    return [UIColor colorWithRed:200 / 255.0f green:0 / 255.0f blue:0 / 255.0f alpha:1.0f];
 }
 
 @end
